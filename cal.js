a210742b36658504163750d67e61d556ee5cadc6
Mathew Ostrander <mathew.ostrander@gmail.com>
2016-01-26 15:50:37 -0600

    file restructure

--- cal.js
+++ cal.js
@@ -1,5 +1,8 @@
 #!/usr/bin/env node
 
 "use strict";
-
+const zellers = require('./lib/zeller.js');
 console.log('hi from cal.js');
+
+//let answerMe = zellers.zellers(2015, 13, 26);
+//console.log(answerMe);
--- tests/test.js
+++ tests/test.js
@@ -1,6 +1,7 @@
 "use strict";
 const { expect } = require('chai');
 const { execSync } = require('child_process');
+const zellers = require('../lib/zeller.js');
 
 describe('cal', () => {
   describe('CLI', () => {
@@ -17,26 +18,67 @@ describe('cal', () => {
 
       //2012, 1 === 13
       it('return 13 for January', () => {
-        const mod = zellars.modifiedMonth(2012, 01);
+        const mod = zellers.modifiedMonth(2012, 1);
 
         expect(mod).to.equal(13);
       })
 
       //2012, 2 === 14
       it('return 14 for February', () => {
-        const mod = zellars.modifiedMonth(2012, 02);
+        const mod = zellers.modifiedMonth(2012, 2);
 
         expect(mod).to.equal(14);
       })
 
       //2012, 3 === 3
       it('return 3 for March', () => {
-        const mod = zellars.modifiedMonth(2012, 03);
+        const mod = zellers.modifiedMonth(2012, 3);
 
         expect(mod).to.equal(3);
       })
+    });
+
+    describe('.modifiedYear', () => {
+      it('returns 2014 for Jan 2015', () => {
+        expect(zellers.modifiedYear(2015, 1)).to.equal(2014);
+      });
+
+      // 2016, 2 === 2015
+      it('returns 2015 for Feb 2016', () => {
+        expect(zellers.modifiedYear(2016, 2)).to.equal(2015);
+      });
+
+      // 2017, 3 === 2017
+      it('returns 2017 for March 2017', () => {
+        expect(zellers.modifiedYear(2017, 3)).to.equal(2017);
+      });
+    });
 
 
+    describe('.getDay', () => {
+      it('returns 2 (Tuesday) for March 1, 2016', () => {
+        expect(zellers.getDay(2016, 3, 1)).to.equal(2);
+      });
+
+      // 2000, 3, 1 === 3
+      it('returns 3 (Wednesday) for March 1, 2000', () => {
+        expect(zellers.getDay(2000, 3, 1)).to.equal(3);
+      });
+
+      // 2100, 3, 1 === 1
+      it('returns 1 (Monday) for March 1, 2100', () => {
+        expect(zellers.getDay(2100, 3, 1)).to.equal(1);
+      });
+
+      // 2200, 3, 2 === 0
+      it('returns 0 (Sunday) for March 2, 2200', () => {
+        expect(zellers.getDay(2200, 3, 2)).to.equal(0);
+      });
+
+      // 2300, 3, 1 === 4
+      it('returns 4 (Thursday) for March 1, 2300', () => {
+        expect(zellers.getDay(2300, 3, 1)).to.equal(4);
+      });
     });
     //.modifiedYear
     //2000, 1 = 1999
