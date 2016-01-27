f56e4fca5eb45b1205d9f762c58c7bb8bb969c3f
Mathew Ostrander <mathew.ostrander@gmail.com>
2016-01-27 13:45:53 -0600

    now I know the days, weeks, and when a month begins

--- tests/test.js
+++ tests/test.js
@@ -6,8 +6,8 @@ const createMonth = require('../lib/month.js');
 const leap = require('../lib/isLeap.js');
 
 describe('cal', () => {
-  describe('CLI', () => {
-    it('should display the current month', () => {
+  xdescribe('CLI', () => {
+   it('should display the current month', () => {
       const goal = execSync('cal').toString();
       const ourOutput = execSync('./cal.js').toString();
 
@@ -100,14 +100,14 @@ describe('cal', () => {
     //2000, 11, 1 === 4
   });
 
-  describe('centering the month', () => {
-    it("should handle January", () => {
- //     expect(center('January 2016')).to.equal('    January 2016');
-    });
-    it("should handle February", () => {
-   //   expect(center('February 2016')).to.equal('   February 2016');
-    });
-  });
+//  describe('centering the month', () => {
+//    it("should handle January", () => {
+//      expect(center('January 2016')).to.equal('    January 2016');
+//    });
+//    it("should handle February", () => {
+//      expect(center('February 2016')).to.equal('   February 2016');
+//    });
+//  });
 
   describe('is the given year a leap year?', () => {
     it("if it isn't a possible leap year (not divisible by 4), return false", () => {
@@ -126,13 +126,44 @@ describe('cal', () => {
       expect(leap.leapYear(1600)).to.be.true;
     });
 
-
   });
+
+  describe('How many days are in a given month?', () => {
+    //30 day month (11/2015)
+    it("if the month has 30 days, func should return 30", () => {
+      expect(createMonth.howManyDays(11, 2015)).to.equal(30);
+    });
+    //31 day month (12/2015)
+    it("if the month has 31 days, func should return 31", () => {
+      expect(createMonth.howManyDays(12, 2015)).to.equal(31);
+    });
+
+    //29 day month (leap year) 2/2012
+    it("if it is a leap year and feb, func should return 29", () => {
+      expect(createMonth.howManyDays(2, 2012)).to.equal(29);
+    });
+
+    //28 day month (feb non leap year) 2/2014
+    it("if it is feb and not a leap year, func should return 28", () => {
+      expect(createMonth.howManyDays(2, 2014)).to.equal(28);
+    });
+  });
+
+  describe('How many weeks are in a given month?', () => {
+    //6 week month (8/2015)
+    it("if the month has 6 weeks, func should return 6", () => {
+      expect(createMonth.weeks(8, 2015)).to.equal(6);
+    });
+    //5 week month (10/2015)
+    it("if the month has 5 weeks, func should return 5", () => {
+      expect(createMonth.weeks(10, 2015)).to.equal(5);
+    });
+
+    //4 week month (2/2015)
+    it("if the month is february and begins on a sunday, (4 weeks) func should return 4", () => {
+      expect(createMonth.weeks(2, 2015)).to.equal(4);
+    });
+  });
+
 });
-//6 week month (8/2015)
-//5 week month (10/2015)
-//4 week month (2/2015)
-//30 day month (11/2015)
-//31 day month (12/2015)
-//29 day month (leap year) 2/2012
-//28 day month (feb non leap year) 2/2014
+
