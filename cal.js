88258e548b003ae2de431437e61edda2c06b4f59
Mathew Ostrander <mathew.ostrander@gmail.com>
2016-01-28 09:00:33 -0600

    makes sure that arguments are valid

--- cal.js
+++ cal.js
@@ -3,24 +3,27 @@
 "use strict";
 const zellers = require('./lib/zeller.js');
 const createMonth = require('./lib/month.js');
+const processArgs = require('./lib/processArgs.js');
 
 const [ , , ...args] = process.argv;
 
 if (args.length === 2) {
-  const month = parseInt(args[0]);
-  const year = parseInt(args[1]);
+  const month = processArgs.checkMonth(args[0]);
+  const year = processArgs.checkYear(parseInt(args[1]));
 
   console.log(`${createMonth.joinOutput(year, month)}`);
 } else if (args.length === 1) {
-  const year = parseInt(args[0]);
+  const year = processArgs.checkYear(parseInt(args[0]));
 
   console.log(`generateYear(${year})`);
-} else {
+} else if (args.length === 0) {
   let timeNow = new Date();
   let thisMonth = timeNow.getMonth()+1;
   let thisYear = timeNow.getFullYear();
 
   console.log(createMonth.joinOutput(thisYear, thisMonth));
+} else {
+  console.log("Syntax not supported: Please enter either a [Year], or a [Month] [Year]");
 }
 
 
